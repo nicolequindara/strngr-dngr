4dcc56a8d2b81cf7b238cef33a72e737d418d600
Nicole Quindara <nicole.quindara@match.com>
2019-03-22 13:33:09 -0500

    [Removing unused components]
    Rm counter & fetchData

--- strngr-dngr/ClientApp/src/components/Layout.js
+++ strngr-dngr/ClientApp/src/components/Layout.js
@@ -1,10 +1,8 @@
-import React from 'react';
 import { Container } from 'reactstrap';
-import NavMenu from './NavMenu';
+import React from 'react';
 
 export default props => (
   <div>
-    <NavMenu />
     <Container>
       {props.children}
     </Container>
