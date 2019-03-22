6d02d679e9c299ecc819562e9572a7da2c66838c
jeremy.bradford <jeremy.bradford@match.com>
2019-03-21 23:28:58 -0500

    add initial image lookup client

---
+++ strngr-dngr/Controllers/PhotoProcessingController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using strngr_dngr.Services;
+
+namespace strngr_dngr.Controllers
+{
+	public class PhotoProcessingController : Controller
+	{
+		private IPhotoProcessingClient _photoClient;
+		public PhotoProcessingController(IPhotoProcessingClient client)
+		{
+			_photoClient = client;
+		}
+
+		[HttpGet("[action]")]
+		public void GetProcessedPhotoData(byte[] imageData)
+		{
+			var photoResults = _photoClient.ProcessPhoto(imageData);
+			//TODO figure out what to return from this
+		}
+	}
+}
